1c9fbe0a133216f1a07624c91b7ce50ed9907d64
Emanuelle Guse <emanuguse@gmail.com>
2023-10-06 03:01:28 +0000

    comments

--- operator.cpp
+++ operator.cpp
@@ -17,17 +17,29 @@ const Certificate& Operator::getCertificate() const {
 }
 
 ByteArray Operator::signPDF(ByteArray pdfByteArray) {
+    // Cria um objeto MessageDigest para calcular o hash do PDF
     MessageDigest messageDigest = MessageDigest(MessageDigest::SHA256);
     ByteArray sha256data = messageDigest.doFinal(pdfByteArray);
+
+    // Realiza a assinatura digital usando o par de chaves privadas do operador
     ByteArray signedPDF = Signer::sign(*keyPair.getPrivateKey(), sha256data, MessageDigest::SHA256);
+
+    // Armazena a assinatura no atributo signedPDF da classe Operator
     this->signedPDF = signedPDF;
+
+    // Retorna a assinatura
     return signedPDF;
 }
 
 bool Operator::verifySignature(ByteArray pdfByteArray) {
+    // Calcula o hash SHA-256 do conteúdo do PDF
     MessageDigest messageDigest = MessageDigest(MessageDigest::SHA256);
     ByteArray sha256data = messageDigest.doFinal(pdfByteArray);
+
+    // Verifica a assinatura digital usando a chave pública do operador
     bool verify = Signer::verify(*keyPair.getPublicKey(), signedPDF, sha256data, MessageDigest::SHA256);
+
+    // Retorna true se a assinatura for válida, senão retorna false
     return verify;
 }
 
